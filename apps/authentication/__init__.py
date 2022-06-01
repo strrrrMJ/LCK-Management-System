a884882718d225a14dc09009f016361f18647a1e
GhostfaceK <445436992@qq.com>
2022-06-01 17:56:53 +0800

    Delete Copyright

--- apps/authentication/__init__.py
+++ apps/authentication/__init__.py
@@ -1,12 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from flask import Blueprint
 
-blueprint = Blueprint(
-    'authentication_blueprint',
-    __name__,
-    url_prefix=''
-)
+blueprint = Blueprint("authentication_blueprint", __name__, url_prefix="")
--- apps/home/routes.py
+++ apps/home/routes.py
@@ -1,8 +1,3 @@
-# -*- encoding: utf-8 -*-
-"""
-Copyright (c) 2019 - present AppSeed.us
-"""
-
 from apps.home import blueprint
 from flask import render_template, request
 from flask_login import login_required
