b1271fc8806e1c9b1fb4f243548c7517b4291374
GhostfaceK <445436992@qq.com>
2022-06-01 20:17:59 +0800

    Delete Unused Page

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -252,51 +252,7 @@ def team_data_specific(team_name):
         )
 
 
-# def tables():
-#     all_playerinfo = PlayerInfo.query.all()
-#     tbody_str = ""
-#     tr_template = "<tr><td class='text-sm font-weight-bold'><img src='static/assets/img/player-pic/{id}.png' class='border-radius-lg' width='60px' /><a href='#'>{id}</a></td><td class='text-xs font-weight-bold'>{realname}</td><td class='text-xs font-weight-bold'>{birthday}</td><td \
-#         class='text-xs font-weight-bold'>{residency}</td></tr>"
-
-#     for playerinfo in all_playerinfo:
-#         tbody_str += tr_template.format(
-#             id=playerinfo.id,
-#             realname=playerinfo.name,
-#             birthday=playerinfo.birthday,
-#             residency=playerinfo.residency,
-#         )
-#     return render_template("home/tables.html", segment="tables", tbody_str=tbody_str)
-
-
-# @blueprint.route("/billing.html")
+# @blueprint.route("/profile.html")
 # @login_required
-# def billing():
-#     return render_template("home/billing.html", segment="billing")
-
-
-@blueprint.route("/notifications.html")
-@login_required
-def notifications():
-    return render_template("home/notifications.html", segment="notifications")
-
-
-@blueprint.route("/profile.html")
-@login_required
-def profile():
-    return render_template("home/profile.html", segment="profile")
-
-
-# # Helper - Extract current page name from request
-# def get_segment(request):
-
-#     try:
-
-#         segment = request.path.split("/")[-1]
-
-#         if segment == "":
-#             segment = "index"
-
-#         return segment
-
-#     except:
-#         return None
+# def profile():
+#     return render_template("home/profile.html", segment="profile")
