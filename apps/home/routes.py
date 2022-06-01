e859fe9d45428ff08847961eb0b9d02c0eeb8bcf
GhostfaceK <445436992@qq.com>
2022-06-01 17:53:16 +0800

    Add Team Rank Card

--- apps/home/routes.py
+++ apps/home/routes.py
@@ -10,12 +10,35 @@ from jinja2 import TemplateNotFound
 from apps.home.models import PlayerInfo, PlayerData, Schedule
 
 
+def getTeamRank(year, season):
+    res = dict()
+    schedule = Schedule.query.filter(
+        (Schedule.year == year) & (Schedule.season == season)
+    ).all()
+    for record in schedule:
+        blue = record.blue_team
+        red = record.red_team
+        winner = record.winner
+        if blue not in res:
+            res[blue] = 0
+        if red not in res:
+            res[red] = 0
+        res[winner] += 1
+    ans = []
+    for team_name, win_cnt in res.items():
+        ans.append((win_cnt, team_name))
+    ans.sort(reverse=True)
+    return ans
+
+
 @blueprint.route("/index.html")
 @login_required
 def index():
+    YEAR = 2022
+    SEASON = "Spring"
     SHOW_DATA_NUM = 7
     high_kda_data_list = (
-        PlayerData.query.filter_by(year=2022, season="Spring")
+        PlayerData.query.filter_by(year=YEAR, season=SEASON)
         .order_by(PlayerData.kda.desc())
         .limit(SHOW_DATA_NUM)
         .all()
@@ -37,7 +60,7 @@ def index():
     }
 
     high_kill_data_list = (
-        PlayerData.query.filter_by(year=2022, season="Spring")
+        PlayerData.query.filter_by(year=YEAR, season=SEASON)
         .order_by(PlayerData.k.desc())
         .limit(SHOW_DATA_NUM)
         .all()
@@ -59,7 +82,7 @@ def index():
     }
 
     high_assistance_data_list = (
-        PlayerData.query.filter_by(year=2022, season="Spring")
+        PlayerData.query.filter_by(year=YEAR, season=SEASON)
         .order_by(PlayerData.a.desc())
         .limit(SHOW_DATA_NUM)
         .all()
@@ -79,7 +102,7 @@ def index():
             },
         ],
     }
-
+    team_rank = getTeamRank(year=YEAR, season=SEASON)
     return render_template(
         "home/index.html",
         segment="index",
@@ -92,6 +115,8 @@ def index():
         best_cp_player=PlayerData.query.filter_by(year=2022, season="Spring")
         .order_by(PlayerData.cp.desc())
         .all()[1],
+        team_rank=team_rank,
+        team_num=len(team_rank),
     )
 
 
@@ -150,6 +175,30 @@ def player_data():
         )
 
 
+@blueprint.route("/player-data.html/<player_id>")
+@login_required
+def player_data_specific(player_id):
+    player_data = (
+        PlayerData.query.filter_by(id=player_id).order_by(PlayerData.year.desc()).all()
+    )
+    if not player_data:
+        return render_template(
+            "home/player-data.html",
+            segment="player-data",
+            search=True,
+            message="This Player Doesn't Exist!",
+        )
+    else:
+        return render_template(
+            "home/player-data.html",
+            segment="player-data",
+            search=False,
+            player_data=player_data,
+            player_team=player_data[0].team_name,
+            player_id=player_data[0].id,
+        )
+
+
 @blueprint.route("/team-data.html", methods=["GET", "POST"])
 @login_required
 def team_data():
@@ -181,6 +230,33 @@ def team_data():
         return render_template("home/team-data.html", segment="team-data", search=True)
 
 
+@blueprint.route("/team-data.html/<team_name>")
+@login_required
+def team_data_specific(team_name):
+    schedule = (
+        Schedule.query.filter(
+            (Schedule.blue_team == team_name) | (Schedule.red_team == team_name)
+        )
+        .order_by(Schedule.year.desc())
+        .all()
+    )
+    if not schedule:
+        return render_template(
+            "home/team-data.html",
+            segment="team-data",
+            search=True,
+            message="This Team Doesn't Exist!",
+        )
+    else:
+        return render_template(
+            "home/team-data.html",
+            segment="team-data",
+            search=False,
+            team_name=team_name,
+            schedule=schedule,
+        )
+
+
 # def tables():
 #     all_playerinfo = PlayerInfo.query.all()
 #     tbody_str = ""
@@ -197,10 +273,10 @@ def team_data():
 #     return render_template("home/tables.html", segment="tables", tbody_str=tbody_str)
 
 
-@blueprint.route("/billing.html")
-@login_required
-def billing():
-    return render_template("home/billing.html", segment="billing")
+# @blueprint.route("/billing.html")
+# @login_required
+# def billing():
+#     return render_template("home/billing.html", segment="billing")
 
 
 @blueprint.route("/notifications.html")
